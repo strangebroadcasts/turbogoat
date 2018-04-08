56acbd06eebda5385fc9172b8c8793b8edcff08f
Jørgen Lund <strangebroadcasts@users.noreply.github.com>
2018-04-08 21:43:26 +0200

    Fix sprite assignment

--- src/sprites.h
+++ src/sprites.h
@@ -4,19 +4,19 @@
 #include "sprites/player.h"
 
 #define PLAYER_TILE_START 0
-#define PLAYER_TILE_END 16
+#define PLAYER_TILE_END 128
 #define PLAYER_SPRITE_START 0
 #define PLAYER_SPRITE_END 16
 
 #include "sprites/bullet.h"
 
-#define BULLET_TILES_START 16
-#define BULLET_PLAYER_TILE 16
-#define BULLET_ENEMY_TILE 17
+#define BULLET_TILES_START 128
+#define BULLET_PLAYER_TILE 128
+#define BULLET_ENEMY_TILE 129
 
 #include "sprites/enemy.h"
 
-#define ENEMY_TILES_START 18
-#define ENEMY_TILES_END 22
+#define ENEMY_TILES_START 130
+#define ENEMY_TILES_END 134
 
 #endif
